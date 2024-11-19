2c2821b8cfafb1cb6e21eb002bfca70657d2db53
Rei <dokorei@msu.edu>
2024-11-19 15:53:00 -0500

    basic checks

--- gs-extend.py
+++ gs-extend.py
@@ -11,9 +11,12 @@ from gradescope_api.client import GradescopeClient
 from gradescope_api.course import GradescopeCourse
 
 settings_path = Path("settings.toml")
+if not settings_path.exists():
+    print("No settings found, be sure to run ./gs-config.py first!")
+    exit(0)
 settings = tomllib.loads(settings_path.read_text())
 if len(settings["courses"]) == 0:
-    print("No courses found, make sure you run ./gs-config.py first!")
+    print("No courses found, make sure you configure a course with ./gs-config.py first!")
     exit(0)
 
 load_dotenv()
@@ -22,9 +25,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--id", choices=settings["courses"], default=settings["default-course"], help="Course identifier")
 parser.add_argument("names", nargs="*", help="student names")
 parser.add_argument("-d", "--days", type=int, default=settings["default-length"], help="Number of days after deadline to extend the assignment. Does not stack with other extensions.")
-parser.add_argument("-s", "--string", help="String for assignment titles to contain (e.g. -s hw4 to apply extension to all assignments with 'hw4' in the title)")
+# probably default to the most recent assignment? for now just leave it as this
+parser.add_argument("-s", "--string", required=True, help="String for assignment titles to contain (e.g. -s hw4 to apply extension to all assignments with 'hw4' in the title)")
 
 args = parser.parse_args()
+if len(args.names) == 0:
+    print("No names supplied, exiting..")
+    exit(0)
 course_info_path = Path(f"{settings['course_path']}/{args.id}.toml")
 course_info = tomllib.loads(course_info_path.read_text())
 roster = course_info["roster"]
@@ -44,6 +51,6 @@ for raw_name in args.names:
         continue
     else:
         email = roster[student_name]
-    print(f"{student_name} ({email})""
+    print(f"{student_name} ({email})")
     for assignment in assignments:
         assignment.apply_extension(roster[student_name], args.days)
